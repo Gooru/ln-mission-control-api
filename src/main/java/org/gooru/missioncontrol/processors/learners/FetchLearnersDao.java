f65f96812c552af3094d06af59a72fe7f06c67d6
sheeban <sheeban@gooru.org>
2020-02-08 13:55:41 +0530

    Added support for search username, email and first and last name

--- src/main/java/org/gooru/missioncontrol/processors/learners/FetchLearnersDao.java
+++ src/main/java/org/gooru/missioncontrol/processors/learners/FetchLearnersDao.java
@@ -11,7 +11,9 @@ interface FetchLearnersDao {
   @Mapper(FetchLearnersModelMapper.class)
   @SqlQuery("SELECT id, username, reference_id, email, first_name, last_name, user_category, thumbnail, gender, about, school_id, school, "
       + "school_district_id, school_district, country_id, country, state_id, state, display_name, "
-      + "roster_global_userid, roster_id from users where ((:query is null or username = :query)) "
-      + "AND (:tenantId is null or tenant_id = :tenantId)  offset :offset limit :limit")
+      + "roster_global_userid, roster_id from users where ((:query is null or username ilike CONCAT('%', :query, '%'))) OR "
+      + "((:query is null or email ilike CONCAT('%', :query, '%'))) OR ((:query is null or first_name ilike CONCAT('%', :query, '%'))) OR "
+      + "((:query is null or last_name ilike CONCAT('%', :query, '%'))) "
+      + "AND (:tenantId is null or tenant_id = :tenantId) AND user_category = 'student'  offset :offset limit :limit")
   List<LearnersModel> fetchLearners(@BindBean UserListCommand.UserListCommandBean command);
 }
